47dcac95c0fc151be40c4168eaf6c0bcfec8639b
peachjelly13 <ananyasharma112233441234@gmail.com>
2024-09-05 16:05:18 +0530

    create utility asyncHanler

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+// you first need to create the app before you configure the app 
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+}))
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true,limit:"16kb"}));
+app.use(express.static("public"));
 
 
 export {app}
